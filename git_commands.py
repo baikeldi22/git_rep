7f5549e4119bcd288a5e9f84674ad73e73c00f5c
baikeldi22 <bak.liashiraliev@gmail.com>
2022-12-19 13:05:53 +0200

    .

---
+++ git_commands.py
@@ -0,0 +1,14 @@
+gin init - .git
+git remote add nane url
+git pull origin master
+
+
+git status 
+git add
+git commit -m 'commit'
+git branch 
+git branch name branch
+checkout name branch
+push origin master 
+resetfilename
+
